23a307d76e504b112def6f4abb6b77be8977fdef
Зенкова Екатерина <89969193+rinapak888@users.noreply.github.com>
2021-09-21 23:33:22 +0300

    Delete task3.py

--- homework_python/task3.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-import string
-
-def create (num):
-    result = ''
-    for i in range (num):
-        result += choice(string.ascii_letters)
-    return result
-print (create(2))
